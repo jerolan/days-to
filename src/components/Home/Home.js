06805726b31a698d2e7d4a12f5c2d9c4de9ec221
Jerome Olvera <jerome.olvera@gmail.com>
2019-02-11 08:56:02 -0600

    Update date handler

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,4 +1,5 @@
-import React, { useState, useRef } from 'react';
+import React, { useRef } from 'react';
+import Router from 'next/router';
 
 import Layout from '../Layout';
 import View from '../View';
@@ -7,14 +8,19 @@ import DatePicker from '../DatePicker';
 
 import * as formatters from '../../formatters';
 
-export default function Home() {
+export default function Home(props) {
   const today = new Date();
+  const date = props.url.query.d ? new Date(props.url.query.d) : new Date();
+  const datesBetween = `${formatters.daysBetween(
+    today,
+    date
+  )} Days to ${date.toDateString()}`;
+
   const inputRef = useRef(null);
-  const [date, setDate] = useState(new Date());
 
   function handleDateChange(date) {
-    if (!date) return setDate(new Date());
-    return setDate(date.toDate());
+    if (!date) return Router.push('/');
+    return Router.push(`/?d=${date.format('YYYY/MM/DD')}`);
   }
 
   function triggerDatePicker(event) {
@@ -22,13 +28,15 @@ export default function Home() {
   }
 
   return (
-    <Layout>
+    <Layout
+      title={datesBetween}
+      description={`Comparing ${today.toDateString()} to ${date.toDateString()}`}
+    >
       <View center="xs" middle="xs">
         <Col xs={12}>
-          <p
-            onClick={triggerDatePicker}
-            className="h1"
-          >{`${formatters.daysBetween(today, date)} Days`}</p>
+          <p onClick={triggerDatePicker} className="h1">
+            {datesBetween}
+          </p>
         </Col>
       </View>
       <DatePicker
