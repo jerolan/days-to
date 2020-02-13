78197657182c216e4cbf0fa8795a857cd4aadc86
jerolan <jerome.olvera@gmail.com>
2020-02-12 22:01:28 -0600

    Update postcss config

--- postcss.config.js
+++ postcss.config.js
@@ -1,18 +1,14 @@
 module.exports = {
-  plugins: [
-    "tailwindcss",
-    "autoprefixer",
-    process.env.NODE_ENV === "production"
-      ? [
-          "@fullhuman/postcss-purgecss",
-          {
-            content: [
-              "./pages/**/*.{js,jsx,ts,tsx}",
-              "./components/**/*.{js,jsx,ts,tsx}"
-            ],
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+    ...(process.env.NODE_ENV === "production"
+      ? {
+          "@fullhuman/postcss-purgecss": {
+            content: ["./components/**/*.{js,jsx,ts,tsx}", "./pages/**/*.{js,jsx,ts,tsx}"],
             defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
           }
-        ]
-      : undefined
-  ]
+        }
+      : {})
+  }
 };
